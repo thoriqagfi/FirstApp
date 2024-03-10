5437e11d74e5c6bca83bd5676f51242092b99676
Agfi <thoriq.aghfi60@gmail.com>
2024-03-10 12:42:58 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Agfi on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
