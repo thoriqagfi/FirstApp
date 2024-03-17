b14c0484d7da6a50b56e31a25b53a087b3efeb2a
Agfi <thoriq.aghfi60@gmail.com>
2024-03-17 19:17:26 +0700

    Section 8: Interfacing with UIKit

---
+++ FirstApp/Views/PageView/PageView.swift
@@ -0,0 +1,30 @@
+//
+//  PageView.swift
+//  FirstApp
+//
+//  Created by Agfi on 16/03/24.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+                .aspectRatio(3 / 2, contentMode: .fit)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map {
+        FeatureCard(landmark: $0)
+    })
+}
